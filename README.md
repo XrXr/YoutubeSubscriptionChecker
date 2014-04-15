0b7eac3fe8c256c08a2cbbadeb1c77a734b0c9fc
XrXr <xrsquared@gmail.com>
2014-04-15 15:57:18 -0400

    Initial commit
    Checking mechanism works, but no UI yet

---
+++ README.md
@@ -0,0 +1,3 @@
+A Firefox add-on that checks for new uploads from Youtube channels.
+
+__Licensed under MPL2__
---
+++ data/setting_page/lib/settings.js
@@ -0,0 +1,4 @@
+angular.module('settings', []).controller('setting-page', function($scope) {
+  $scope.a = 1;
+  $scope.b = 2;
+});
