ca82b97332fe542d466ad3c07d82312bb53acb18
XrXr <xrsquared@gmail.com>
2014-04-22 01:10:29 -0400

    Channel searching.
    TODO: add loading symbol when searching

--- data/hub/app/app.js
+++ data/hub/app/app.js
@@ -31,12 +31,38 @@ angular.module('subscription_checker', ['ngRoute','ngAnimate', 'ui.bootstrap'])
             });
         };
         $scope.params = $routeParams;
-        $scope.channels = [{name:"LinusTechTips"}, {name:"sxephil"}, {name:"SourceFed"}];
+        $scope.channels = [{name:"LinusTechTips"}, {name:"sxephil"}, {name:"sxephil"}, {name:"SourceFed"}];
     })
     .controller('subscription', function($scope) {
         $scope.a = 100;
         $scope.b = 200;
         $scope.videos = [1,2,3,4,5,6,7,8,9,10,11,12,13,12,15,16,17,18,19,20];
+    })
+    .directive("bindHeight", function() {
+        return {
+            link: function(scope, iElement, iAttrs) {
+                scope.$watch(
+                    function() {
+                        return iElement[0].clientHeight;},
+                    function(newVal, oldVal) {
+                        scope[iAttrs.bindHeight] = newVal;
+                    }
+                );    
+            }
+        };
+    })
+    .directive("bindWidth", function() {
+        return {
+            link: function(scope, iElement, iAttrs) {
+                scope.$watch(
+                    function() {
+                        return iElement[0].clientWidth;},
+                    function(newVal, oldVal) {
+                        scope[iAttrs.bindWidth] = newVal;
+                    }
+                );    
+            }
+        };
     });
 
 function settings($scope, $modalInstance) {
@@ -51,6 +77,9 @@ function settings($scope, $modalInstance) {
 
 function subscriptions($scope, $modalInstance, $modal, channels) {
     $scope.channels = channels;
+    $scope.fit = function(body_height, result_height) {
+        return {height: Math.max(body_height, result_height + 10) + 'px'};
+    };
     $scope.save = function () {
         $modalInstance.close();
     };
@@ -64,6 +93,27 @@ function subscriptions($scope, $modalInstance, $modal, channels) {
             var event = new CustomEvent('subscriptions');
             event.initCustomEvent("search", true, true, {});
             document.documentElement.dispatchEvent(event); // tell content script to start the search
+            // $scope.search_result = [{title: "cactus", thumbnail: "http://placekitten.com/200/200"},
+            //     {title: "cactuasdasdasdasdasdasdasdaasdasdasdasdsds", thumbnail: "http://placekitten.com/200/200"},
+            //     {title: "cactus", thumbnail: "http://placekitten.com/200/200"}
+            // ];
         }
+        document.documentElement.addEventListener("search-result", function(event) {
+            // $scope.search_result = [{title: "cactus", thumbnail: "http://placekitten.com/200/200"},
+            //         {title: "cactuasdasdasdasdasdasdasdaasdasdasdasdsds", thumbnail: "http://placekitten.com/200/200"},
+            //         {title: "cactus", thumbnail: "http://placekitten.com/200/200"}
+            //     ];
+            $scope.search_result = JSON.parse(event.detail);
+            $scope.$apply();
+        }, false);
     };
+}
+
+function clone(obj) {
+    if (null === obj || "object" != typeof obj) return obj;
+    var copy = obj.constructor();
+    for (var attr in obj) {
+        if (obj.hasOwnProperty(attr)) copy[attr] = obj[attr];
+    }
+    return copy;
 }
--- data/hub/app/content.js
+++ data/hub/app/content.js
@@ -5,3 +5,12 @@ document.documentElement.addEventListener("search", function(event) {
 function start_search() {
     self.port.emit("search-channel", document.getElementById('channel_search').value);
 }
+
+self.port.on('search-result', function(pay_load) {
+    var result = JSON.stringify(pay_load); 
+    // passing pay_load as reference directly would result in cross-origin problems
+    // passing the stringified version circumvents it.
+    var result_event = new CustomEvent('subscriptions');
+    result_event.initCustomEvent("search-result", true, true, result);
+    document.documentElement.dispatchEvent(result_event);
+});
