88e0fa435841b7d40f9e6d0e5c6735bfbc0b7a02
XrXr <xrsquared@gmail.com>
2014-06-24 10:40:15 -0400

    Pre-release chores
    - Remove debug lines
    - Update to Angular 1.2.18
    - Switch to local minified version
    - Remove extra files
    - Slight change of order in check_all()

--- README.md
+++ README.md
@@ -1,3 +1,15 @@
 A Firefox add-on that checks for new uploads from Youtube channels.
 
 __Licensed under MPL2__
+
+## Modified AngularJS
+
+The AngularJS included in this add-on is slightly modified.
+```js
+    {cache: $templateCache}
+```
+become
+```js
+    {cache: $templateCache, responseType: "text"}
+```
+to squelch a error message Firefox log due to fetching templates from `file:///`
--- data/hub/app/content.js
+++ data/hub/app/content.js
@@ -1,3 +1,10 @@
+/*
+This Source Code Form is subject to the terms of the
+Mozilla Public License, v. 2.0.
+If a copy of the MPL was not distributed with this file,
+You can obtain one at http://mozilla.org/MPL/2.0/.
+Author: XrXr
+*/
 function send_dom_event (type, name, data) {
     // passing pay_load as reference directly would result in cross-origin problems
     // passing the stringified version circumvents it.
