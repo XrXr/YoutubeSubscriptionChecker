6b57af0eb2192bf82db6a285d78877691339e644
XrXr <xrsquared@gmail.com>
2014-07-04 22:20:05 -0400

    Display video count in button icon. Change id
    - Not the best looking icon and number, but it will have to do

--- data/sound/on_demand.js
+++
@@ -1,4 +0,0 @@
-var sound = new Audio('notification.ogg');
-self.port.on("play", function() {
-    sound.play();
-});
---
+++ data/utility/on_demand.js
@@ -0,0 +1,51 @@
+/*
+This Source Code Form is subject to the terms of the
+Mozilla Public License, v. 2.0.
+If a copy of the MPL was not distributed with this file,
+You can obtain one at http://mozilla.org/MPL/2.0/.
+Author: XrXr
+*/
+var sound = new Audio('notification.ogg');
+self.port.on("play", function() {
+    sound.play();
+});
+
+self.port.on("draw", draw);
+
+function draw (number) {
+    var length = (number + "").length;
+    // change the svg content
+    var svg32 = document.getElementById('32x32');
+    var svg64 = document.getElementById('64x64');
+    var text_node = svg32.querySelector("text");
+    text_node.textContent = number + "";
+
+    text_node = svg64.querySelector("text");
+    //start drawing
+    var canvas_32 = document.getElementById("32c");
+    var canvas_64 = document.getElementById("64c");
+    Promise.all([draw_in_canvas(canvas_32, svg32),
+                 draw_in_canvas(canvas_64, svg64)]).
+        then(function(uris) {
+            self.port.emit("icons", {"32":uris[0], "64":uris[1]});
+        });
+}
+
+function draw_in_canvas (canvas_node, svg_node) {
+    var ctx = canvas_node.getContext('2d');
+
+    var data = svg_node.outerHTML;
+
+    var img = new Image();
+    var svg_blob = new Blob([data], {type: 'image/svg+xml;charset=utf-8'});
+    var url = window.URL.createObjectURL(svg_blob);
+    img.src = url;
+
+    return new Promise(function(resolve) {
+        img.onload = function () {
+          ctx.drawImage(img, 0, 0);
+          window.URL.revokeObjectURL(url);
+          resolve(canvas_node.toDataURL());
+        };
+    });
+}
