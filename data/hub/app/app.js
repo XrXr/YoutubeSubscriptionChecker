XrXr/YoutubeSubscriptionChecker

commit 686ee0ed70a0ff0e513ec187637424696617257a

Feedback for sucessful import. Call $apply() when receiving Bridge events
- Remove unnecessary third parameter for calls to Bridge.on()
- Transform some functions to use arrow function syntax
- Indentation fix for Bridge.on()

data/hub/app/app.js
@@ -150,9 +150,7 @@ angular.module('subscription_checker', ['ngAnimate', 'ui.bootstrap'])
                     function make_clone (e) {
                         if (!e["$$NG_REMOVED"]) {
                             var clone = angular.element(e).clone();
-                            clone.on("animationend", function() {
-                                clone.remove();
-                            });
+                            clone.on("animationend", () => clone.remove());
                             // save angular some work
                             clone.removeAttr("masonry-tile");
                             angular.element(document.querySelector("#dummy")).append(clone);
@@ -320,13 +318,16 @@ angular.module('subscription_checker', ['ngAnimate', 'ui.bootstrap'])
       Responsible for communication with the add-on. This factory guarantees
       that only one listener is register for any given event name at a time
     */
-    .factory("Bridge", function() {
+    .factory("Bridge", function($rootScope) {
         var registered = {};
         function on (name, listener) {
-                document.documentElement.
-                    removeEventListener(name, registered[name]);
-                registered[name] = listener;
-                document.documentElement.addEventListener(name, listener);
+            document.documentElement.
+                removeEventListener(name, registered[name]);
+            registered[name] = listener;
+            document.documentElement.addEventListener(name, event => {
+                listener(event);
+                $rootScope.$apply();
+            });
         }
 
         function emit (name, data) {
@@ -594,15 +595,13 @@ angular.module('subscription_checker', ['ngAnimate', 'ui.bootstrap'])
             refresh_masonry();
         };
 
-        Bridge.on("open-settings", event => {
-            $scope.open_settings();
-        });
+        Bridge.on("open-settings", () => $scope.open_settings());
 
-        Bridge.on("subscribed-channels", function(event) {
+        Bridge.on("subscribed-channels", event => {
             if (ChannelList.update_channels(JSON.parse(event.detail))) {
                 $scope.open_subscriptions();
             }
-        }, false);
+        });
     })
 
     .controller('videos', function($scope, $timeout, VideoStorage, ChannelList, Bridge) {
@@ -626,20 +625,19 @@ angular.module('subscription_checker', ['ngAnimate', 'ui.bootstrap'])
             }
         };
 
-        Bridge.on("videos", function(event) {
+        Bridge.on("videos", event => {
             var details = JSON.parse(event.detail);
             VideoStorage.new_main(details[0]);
             VideoStorage.new_history(details[1]);
             VideoStorage.switch_to("main");
             ChannelList.current_channel = "";
             ChannelList.update_video_count();
-            $scope.$apply(refresh_masonry);
+            refresh_masonry();
         });
 
-        Bridge.on("duration-update", function(event) {
+        Bridge.on("duration-update", event => {
             var detail = JSON.parse(event.detail);
             VideoStorage.update_duration(detail.id, detail.duration);
-            $scope.$apply();
         });
     })
 
@@ -740,9 +738,12 @@ angular.module('subscription_checker', ['ngAnimate', 'ui.bootstrap'])
         };
 
         $scope.tabs.import_export = {
+            import_success: "",
+            import_error: "",
             export_settings: () => Bridge.emit("export", null),
             import_settings: input => {
                 $scope.tabs.import_export.import_error = false;
+                $scope.tabs.import_export.import_success = false;
                 Bridge.emit("import", input);
             }
         };
@@ -761,15 +762,14 @@ angular.module('subscription_checker', ['ngAnimate', 'ui.bootstrap'])
             $modalInstance.dismiss('cancel');
         };
 
-        Bridge.on("export-result", event => {
-            $scope.tabs.import_export.config_output = event.detail;
-            $scope.$apply();
-        }, false);
+        Bridge.on("export-result", event =>
+            $scope.tabs.import_export.config_output = event.detail);
 
-        Bridge.on("import-error", event => {
-            $scope.tabs.import_export.import_error = true;
-            $scope.$apply();
-        }, false);
+        Bridge.on("import-error", () =>
+            $scope.tabs.import_export.import_error = true);
+
+        Bridge.on("import-success", () =>
+            $scope.tabs.import_export.import_success = true);
     })
 
     .controller("subscriptions", function ($scope, $modalInstance, ChannelList, VideoStorage, Bridge) {
@@ -809,7 +809,6 @@ angular.module('subscription_checker', ['ngAnimate', 'ui.bootstrap'])
             function duplicate_listener () {
                 Bridge.removeListener("channel-added");
                 $scope.duplicate = true;
-                $scope.$apply();
             }
             Bridge.on("channel-added", add_listener);
             Bridge.on("channel-duplicate", duplicate_listener);
@@ -843,7 +842,7 @@ angular.module('subscription_checker', ['ngAnimate', 'ui.bootstrap'])
             if($event.keyCode == 13) {
                 $scope.search.in_progress = true;
                 Bridge.emit("search-channel", $scope.search.term);
-                Bridge.on("search-result", search_result_listener, false);
+                Bridge.on("search-result", search_result_listener);
                 $scope.search.result = [];
                 $scope.search.searched_once = true;
                 $scope.duplicate = false;

data/hub/app/bridge.js
@@ -36,7 +36,7 @@ the_page_script.sends(
     ]).recieves(
     ["open-settings", "videos", "config", "search-result",
      "subscribed-channels", "channel-added", "channel-duplicate",
-     "duration-update", "import-error", "export-result", "import-result"
+     "duration-update", "import-error", "export-result", "import-success"
     ]);
 
 self.port.emit("get-videos", null);  // get all videos once contentscript loads

data/hub/partials/settings.html
@@ -116,6 +116,7 @@
     <!-- filters related end-->
     <!-- import related start-->
     <div class="alert alert-danger setting-warning" ng-if="tabs.import_export.import_error">Import failed (malformed data?)</div>
+    <div class="alert alert-success setting-warning" ng-if="tabs.import_export.import_success">Import successfully finished</div>
     <!-- import related end-->
     <button class="btn btn-primary" ng-click="save()" ng-disabled="!tabs.general.valid">Save</button>
     <button class="btn btn-warning" ng-click="cancel()">Cancel</button>

todo.TODO
@@ -51,7 +51,7 @@
    x shorter string for import/export @cancelled (14-10-31 17:43)
    √ channel video count not updated in history mode when clearing history @done (14-12-14 18:37)
    √ namespace each tab in settings modal @done (14-12-14 18:02)
-   □ more UI feedback for successful import
-     □ directive for notification banners in settings tab
+   √ more UI feedback for successful import @done (14-12-15 23:57)
+     x directive for notification banners in settings tab @cancelled (14-12-15 23:57)
    □ masonry service/factory
    □ write full testing procedure

commit 0cd78e4dcc7f5ec586328b163edc5d1e98302f30

Fix event listener not being removed in Brige
- This is a regression introduced by
  686ee0ed70a0ff0e513ec187637424696617257a. Since the listener is wrapped,
  remove won't work since it's called with the un-wrapped listener

data/hub/app/app.js
@@ -320,13 +320,14 @@ angular.module('subscription_checker', ['ngAnimate', 'ui.bootstrap'])
     .factory("Bridge", function($rootScope) {
         var registered = {};
         function on (name, listener) {
-            document.documentElement.
-                removeEventListener(name, registered[name]);
-            registered[name] = listener;
-            document.documentElement.addEventListener(name, event => {
+            function wrapper (event) {
                 listener(event);
                 $rootScope.$apply();
-            });
+            }
+            document.documentElement.
+                removeEventListener(name, registered[name], false);
+            document.documentElement.addEventListener(name, wrapper, false);
+            registered[name] = wrapper;
         }
 
         function emit (name, data) {
@@ -337,14 +338,18 @@ angular.module('subscription_checker', ['ngAnimate', 'ui.bootstrap'])
         function once (name, listener) {
             function wrapper () {
                 listener();
-                documente.documentElement.removeEventListener(name, wrapper);
+                document.documentElement
+                    .removeEventListener(name, wrapper, false);
+                registered[name] = null;
+                $rootScope.$apply();
             }
-            on(name, wrapper);
+            registered[name] = wrapper;
+            document.documentElement.addEventListener(name, wrapper);
         }
 
         function removeListener (name) {
             document.documentElement.
-                removeEventListener(name, registered[name]);
+                removeEventListener(name, registered[name], false);
             registered[name] = null;
         }
 
@@ -800,17 +805,11 @@ angular.module('subscription_checker', ['ngAnimate', 'ui.bootstrap'])
             $scope.duplicate = false;
             Bridge.emit("add-channel", channel);
 
-            function add_listener () {
-                Bridge.removeListener("channel-duplicate");
-                ChannelList.channels.push(channel);
-            }
-
-            function duplicate_listener () {
-                Bridge.removeListener("channel-added");
-                $scope.duplicate = true;
-            }
-            Bridge.on("channel-added", add_listener);
-            Bridge.on("channel-duplicate", duplicate_listener);
+            Bridge.once("channel-added", () => {
+                console.log("for ",channel);
+                ChannelList.channels.push(channel)
+            });
+            Bridge.once("channel-duplicate", () => $scope.duplicate = true);
         };
 
         $scope.remove_channel = function(channel) {