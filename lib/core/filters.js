XrXr/YoutubeSubscriptionChecker

commit 1b22ddeb6e977bc13a63270cd496dee6016bdffd

Better include filter logic. Add relevant tests
- "Include only" filters use to be appied in order. For example two filters
  that includes "a" and "b" working on videos titled "a and "b" would only
  include "a". With this change, both videos will be included
- Include filters are applied before exclude filters
- Refactor filter tests

Closes #12

lib/core/filters.js
@@ -64,32 +64,50 @@ function Filter (channel_title, video_title_pattern,
 Filter.arg_names = raw_filter_expectations.map(a => a[0]);
 Filter.property_names = full_filter_expectations.map(a => a[0]);
 
+// test whether a video match with conditions inside a filter
+function test_filter (filter, video) {
+    let regex;
+    if (filter.video_title_is_regex) {
+        regex = new RegExp(filter.video_title_pattern, "i");
+    }
+    let title = video.title.toLowerCase();
+    return regex ? regex.test(title) :
+                   title.contains(filter.video_title_pattern);
+}
+
 // put a group of videos from the same channel through filters,
 // return [[include], [exclude]]
 function filter_videos (videos, filters) {
     let include = videos;
     let exclude = [];
-    filters = filters || [];
-    for(let filter of filters) {
-        let regex;
-        if (filter.video_title_is_regex) {
-            regex = new RegExp(filter.video_title_pattern, "i");
+    let include_filters = [];
+    let exclude_filters = [];
+    for (let filter of filters) {
+        let target = filter.include_on_match ? include_filters :
+                                               exclude_filters;
+        target.push(filter);
+    }
+    let has_include_filters = include_filters.length > 0;
+    let ordered_filters = include_filters.concat(exclude_filters);
+    if (has_include_filters) {
+        [include, exclude] = [exclude, include];
+    }
+    for (let filter of ordered_filters) {  // first apply the include filters
+        let take_from = include;
+        let put_to = exclude;
+        let should_transfer = test_filter.bind(null, filter);
+        if (filter.include_on_match) {
+            [put_to, take_from] = [take_from, put_to];
         }
         let result = [];
-        for (let i = include.length - 1; i >= 0; i--) {
-            let passed;
-            let title = videos[i].title.toLowerCase();
-            passed = regex ? regex.test(title) :
-                             title.contains(filter.video_title_pattern);
-            if (filter.include_on_match) {
-                passed = !passed;
-            }
-            if (passed) {
-                result.unshift(videos[i]);
-                include.splice(i, 1);
+        for (let i = take_from.length - 1; i >= 0; i--) {
+            if (should_transfer(take_from[i])) {
+                result.unshift(take_from[i]);
+                take_from.splice(i, 1);
             }
         }
-        exclude = exclude.concat(result);
+        // results from filters should be in same order as the filters
+        put_to.push(...result);
     }
     return [include, exclude];
 }

test/test-filter-videos.js
@@ -27,12 +27,16 @@ function get_samples () {
                       Video("happIness"), Video("greatness Awaits")];
 }
 
+function serialize (result) {
+    return [result[0].map(get_title), result[1].map(get_title)];
+}
+
 exports["test filter_videos() include"] = {
     'test single inclusive filter(non-regex)': assert => {
         let filter = Filter("", "greatness", false, true);
         let videos = get_samples();
         let result = filter_videos(videos, [filter]);
-        let result_serialized = [result[0].map(get_title), result[1].map(get_title)];
+        let result_serialized = serialize(result);
         let expect = [["greatness", "greatness awaits"],
                       ["great", "bad", "happiness"]];
         assert.deepEqual(result_serialized, expect,
@@ -42,7 +46,7 @@ exports["test filter_videos() include"] = {
         let filter = Filter("", "(^gr|^h)", true, true);
         let videos = get_samples();
         let result = filter_videos(videos, [filter]);
-        let result_serialized = [result[0].map(get_title), result[1].map(get_title)];
+        let result_serialized = serialize(result);
         let expect = [["great", "greatness", "happiness", "greatness awaits"],
                       ["bad"]];
         assert.deepEqual(result_serialized, expect,
@@ -53,9 +57,9 @@ exports["test filter_videos() include"] = {
         let filter_b = Filter("", "ness", false, true);
         let videos = get_samples();
         let result = filter_videos(videos, [filter_a, filter_b]);
-        let result_serialized = [result[0].map(get_title), result[1].map(get_title)];
-        let expect = [["greatness", "greatness awaits"],
-                      ["bad", "happiness", "great"]];
+        let result_serialized = serialize(result);
+        let expect = [["great", "greatness", "greatness awaits", "happiness"],
+                      ["bad"]];
         assert.deepEqual(result_serialized, expect,
                          "multiple includes applied properly");
     },
@@ -63,7 +67,7 @@ exports["test filter_videos() include"] = {
         let filter = Filter("", "great", false, false);
         let videos = get_samples();
         let result = filter_videos(videos, [filter]);
-        let result_serialized = [result[0].map(get_title), result[1].map(get_title)];
+        let result_serialized = serialize(result);
         let expect = [["bad",  "happiness"],
                       ["great", "greatness", "greatness awaits"]];
         assert.deepEqual(result_serialized, expect,
@@ -74,11 +78,23 @@ exports["test filter_videos() include"] = {
         let filter_b = Filter("", "great", false, false);
         let videos = get_samples();
         let result = filter_videos(videos, [filter_a, filter_b]);
-        let result_serialized = [result[0].map(get_title), result[1].map(get_title)];
+        let result_serialized = serialize(result);
         let expect = [["bad"],
                       ["happiness", "great", "greatness", "greatness awaits"]];
         assert.deepEqual(result_serialized, expect,
                          "multiple exludes applied properly");
+    },
+    'test filter application order': assert => {
+        let videos = get_samples();
+        let result = filter_videos(videos, [Filter("", "ness", false, true),
+                                            Filter("", "great", false, true),
+                                            Filter("", "awaits", false, false),
+                                            Filter("", "happ", false, false)]);
+        let result_serialized = serialize(result);
+        let expect = [["greatness", "great"],
+                      ["bad", "greatness awaits", "happiness"]];
+        assert.deepEqual(result_serialized, expect,
+                         "filters applied in correct order");
     }
 };
 

commit 85708e817544b27971ff7d329f13b30b64e57add

Handle channels that have undefined filter list in filter_videos (regression)
- This regression was introduced in
  1b22ddeb6e977bc13a63270cd496dee6016bdffd

lib/core/filters.js
@@ -79,6 +79,7 @@ function test_filter (filter, video) {
 // put a group of videos from the same channel through filters,
 // return [[include], [exclude]]
 function filter_videos (videos, filters) {
+    if (!filters) return [[],[]];
     let include = videos;
     let exclude = [];
     let include_filters = [];