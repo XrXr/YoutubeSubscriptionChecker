b3a4ced35d9b5c98ea68e74911bf86a30949375e
XrXr <xrsquared@gmail.com>
2017-05-22 23:46:10 -0400

    Oauth proto

--- extension/src/main.js
+++ extension/src/main.js
@@ -4,25 +4,37 @@ Mozilla Public License, v. 2.0.
 If a copy of the MPL was not distributed with this file,
 You can obtain one at http://mozilla.org/MPL/2.0/.
 Author: XrXr
-
 */
+
 import * as request from "./youtube/request";
 
-request.search_channel()
-    .then(r => {
-        // browser.notifications.create(null, {
-        //     "type": "basic",
-        //     "title": "YoutubeSubscriptionChecker",
-        //     "message": JSON.stringify(r)
-        // });
-        console.log(r)
-    });
+// request.search_channel()
+//     .then(r => {
+//         browser.notifications.create(null, {
+//             "type": "basic",
+//             "title": "YoutubeSubscriptionChecker",
+//             "message": JSON.stringify(r)
+//         });
+//     });
 
+function logURL(requestDetails) {
+  console.log("Loading: " + requestDetails.url);
+  browser.tabs.update(requestDetails.tabId, {url: chrome.extension.getURL("hub/home.html")});
+  return {cancel:true}
+}
 
-        browser.notifications.create(null, {
-            "type": "basic",
-            "title": "YoutubeSubscriptionChecker",
-            "message": "sd"
-        });
+browser.webRequest.onBeforeRequest.addListener(
+  logURL,
+  {urls: ["https://youtube-subscription-checker/*"], types:["main_frame"]},
+  ["blocking"]
+);
+
+browser.notifications.create(null, {
+    "type": "basic",
+    "title": "YoutubeSubscriptionChecker",
+    "message": "Add-on loaded"
+});
 
 window.request = request
+
+
